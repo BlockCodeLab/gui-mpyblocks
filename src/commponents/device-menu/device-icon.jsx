a37d075b3bbe5abfce8ceba9b1d95482cfbc5536
Yeqin Gong <gon@gyeq.in>
2025-01-06 00:29:51 +0800

    修改硬件连接状态上下位位置

--- src/commponents/device-menu/device-icon.jsx
+++ src/commponents/device-menu/device-icon.jsx
@@ -4,6 +4,6 @@ import deviceIcon from './icon-device.svg';
 import deviceConnectedIcon from './icon-device-connected.svg';
 
 export function DeviceIcon() {
-  const { device } = useAppContext();
-  return device.value ? <img src={deviceConnectedIcon} /> : <img src={deviceIcon} />;
+  const { appState } = useAppContext();
+  return appState.value?.device ? <img src={deviceConnectedIcon} /> : <img src={deviceIcon} />;
 }
